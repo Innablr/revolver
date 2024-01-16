930b529240bc8a80452161d64baacee2995a75f3
Alex Lutman <alex.lutman@innablr.com.au>
2024-01-16 12:31:29 +1100

    Update linter configuration, add secret linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,40 @@
 module.exports = {
+  env: {
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
+    'plugin:json/recommended',
+  ],
+  overrides: [
+    {
+      files: '*.js',
+      rules: {
+        'prettier/prettier': ['warn'],
+      }
+    },
+    {
+      files: '*.json',
+      rules: {
+        'json/*': ['warn']
+      }
+    },
+    {
+      files: 'test/**',
+      plugins: ['mocha'],
+      extends: [
+        'plugin:mocha/recommended',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        'mocha/no-setup-in-describe': ['off'],
+        'prettier/prettier': ['warn'],
+      }
+    }
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
