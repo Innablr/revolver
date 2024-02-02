1411531ced8146f1b6cbb212a52360a3483db7c6
Roberts, Simon <Simon.Roberts@anz.com>
2024-02-02 11:13:16 +1100

    Simple test for config parser

--- test/config/config.spec.ts
+++ test/config/config.spec.ts
@@ -3,12 +3,16 @@ import { RevolverConfig } from '../../lib/config';
 import path from 'path';
 
 describe('Validate example config', function () {
-  it('Check defaults', async function () {
+  it('Check simple parsing', async function () {
     const rc = new RevolverConfig();
     const config = await rc.readConfigFromFile(path.join(__dirname, 'revolver-config-example.yaml'));
     expect(config.defaults.settings.region).to.equal('ap-southeast-2');
     expect(config.defaults.settings.timezoneTag).to.equal('Timezone');
     expect(config.defaults.drivers[0].name).to.equal('ec2');
-    expect(config.accounts.excludeList).to.be.an( "array" ).that.is.not.empty;
+    expect(config.defaults.drivers[0].active).to.equal(true);
+    expect(config.defaults.drivers[0].pretend).to.equal(false);
+    expect(config.accounts.includeList).to.have.lengthOf(1);
+    expect(config.accounts.includeList[0].accountId).to.equal(222222222222);
+    expect(config.accounts.excludeList).to.deep.equal([]);
   });
 });
