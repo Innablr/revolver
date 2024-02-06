b777caff4141fc10954a91ddd95e3fe6f346bafb
alutman-innablr <63215484+alutman-innablr@users.noreply.github.com>
2024-02-06 14:24:14 +1100

    Add account ID to audit log (#138)

--- actions/audit.ts
+++ actions/audit.ts
@@ -3,6 +3,7 @@ import { promises as fs } from 'node:fs';
 
 export interface ActionAuditEntry {
   time: DateTime;
+  accountId: string;
   plugin: string;
   driver: string;
   resourceType: string;
@@ -35,11 +36,11 @@ export class ActionAuditLogCSV extends ActionAuditLog {
     if (this.append) mode = 'a';
     fs.open(this.outputFile, mode).then(async (f) => {
       if (!this.append) {
-        await f.write(`time,plugin,driver,resourceType,resourceId,action,status,reason\n`);
+        await f.write(`time,accountId,plugin,driver,resourceType,resourceId,action,status,reason\n`);
       }
       for (const e of this.entries) {
         const reason = `"${e.reason.replaceAll('"', '""')}"`;
-        const line = `${e.time},${e.plugin},${e.driver},${e.resourceType},${e.resourceId},${e.action},${e.status},${reason}\n`;
+        const line = `${e.time},${e.accountId},${e.plugin},${e.driver},${e.resourceType},${e.resourceId},${e.action},${e.status},${reason}\n`;
         await f.write(line);
       }
     });
@@ -52,10 +53,9 @@ export class ActionAuditLogConsole extends ActionAuditLog {
   }
 
   process() {
-    console.log(`${'PLUGIN'.padEnd(20)}${'DRIVER'.padEnd(25)}${'TYPE'.padEnd(5)}${'ID'.padEnd(40)}${'ACTION'.padEnd(10)}${'STATUS'.padEnd(10)}${'REASON'}`)
+    console.log(`${'ACCOUNT_ID'.padEnd(16)}${'PLUGIN'.padEnd(20)}${'DRIVER'.padEnd(25)}${'TYPE'.padEnd(5)}${'ID'.padEnd(40)}${'ACTION'.padEnd(10)}${'STATUS'.padEnd(10)}${'REASON'}`)
     for (const e of this.entries) {
-      const reason = `"${e.reason.replaceAll('"', '""')}"`;
-      const line = `${e.plugin.padEnd(20)}${e.driver.padEnd(25)}${e.resourceType.padEnd(5)}${e.resourceId.padEnd(40)}${e.action.padEnd(10)}${e.status.padEnd(10)}${reason}`;
+      const line = `${e.accountId.padEnd(16)}${e.plugin.padEnd(20)}${e.driver.padEnd(25)}${e.resourceType.padEnd(5)}${e.resourceId.padEnd(40)}${e.action.padEnd(10)}${e.status.padEnd(10)}${e.reason}`;
       console.log(line);
     }
   }
