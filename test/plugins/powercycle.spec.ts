05d584c493cffab231cc310e05f33a74f9a03d21
Roberts, Simon <Simon.Roberts@anz.com>
2024-02-26 18:57:20 +1100

    Add basic exercise for powercycle plug. Rename test data files for clarity.

--- test/plugins/powercycle.spec.ts
+++ test/plugins/powercycle.spec.ts
@@ -6,10 +6,9 @@ import { handler as revolverHandle } from '../../revolver';
 import environ from '../../lib/environ';
 import * as fs from 'fs';
 
-const LOCAL_CONFIG = path.join(__dirname, 'test-revolver-config.powercycle.yaml');
-// const RESOURCES_FILE = path.join(__dirname, 'resources.json'); // in config YAML
+const LOCAL_CONFIG = path.join(__dirname, 'powercycle.config.yaml');
 const OUTPUT_AUDIT_CSV_FILE = path.join(__dirname, 'audit.csv');
-// const OUTPUT_RESOURCES_CSV_FILE = path.join(__dirname, 'resources.csv');
+const OUTPUT_RESOURCES_CSV_FILE = path.join(__dirname, 'resources.csv');
 const OUTPUT_RESOURCES_JSON_FILE = path.join(__dirname, 'resources.json');
 
 const timeStamp = '2024-02-22T23:45:19.521Z';
@@ -42,12 +41,12 @@ const context: Context = {
 };
 
 describe('XXX Run powercycle full cycle', function () {
-  // delete output files before run
-  if (fs.existsSync(OUTPUT_AUDIT_CSV_FILE)) fs.unlinkSync(OUTPUT_AUDIT_CSV_FILE);
-  if (fs.existsSync(OUTPUT_RESOURCES_JSON_FILE)) fs.unlinkSync(OUTPUT_RESOURCES_JSON_FILE);
-  environ.configPath = LOCAL_CONFIG;
-
-  logger.info('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+  beforeEach(function() {
+    // delete output files before run
+    if (fs.existsSync(OUTPUT_AUDIT_CSV_FILE)) fs.unlinkSync(OUTPUT_AUDIT_CSV_FILE);
+    if (fs.existsSync(OUTPUT_RESOURCES_JSON_FILE)) fs.unlinkSync(OUTPUT_RESOURCES_JSON_FILE);
+    environ.configPath = LOCAL_CONFIG;
+  });
 
   it('resolves', (done) => {
     const r = revolverHandle(event, context, () => {});
@@ -56,29 +55,32 @@ describe('XXX Run powercycle full cycle', function () {
         // validate audit.csv
         logger.info(`TEST validating ${OUTPUT_AUDIT_CSV_FILE}`);
         const auditCsvText = fs.readFileSync(OUTPUT_AUDIT_CSV_FILE, 'utf-8');
-        expect((auditCsvText.match(/2024-02-/g) || []).length).to.equal(4); // number of rows
-        expect(auditCsvText).to.include(',ec2,ec2,i-0c688d35209d7f436,stop,');
-        expect(auditCsvText).to.include(',ec2,ec2,i-031635db539857721,stop,');
-        expect(auditCsvText).to.not.include(',ec2,ec2,i-05b6baf37fc8f9454,stop,');
+        // expect((auditCsvText.match(/2024-02-/g) || []).length).to.equal(4); // number of rows
+        expect(auditCsvText).to.include('i-0c688d35209d7f436,stop,pretend,Availability 0x7');
+        expect(auditCsvText).to.include('i-0c688d35209d7f436,setTag,pretend,ReasonSchedule:Availability 0x7');
+        expect(auditCsvText).to.include('i-01531c2e601f21910,start,pretend,Availability 24x7');
+        // expect(auditCsvText).to.not.include(',ec2,ec2,i-05b6baf37fc8f9454,stop,');
 
         // TODO: validate resources.csv
+        // logger.info(`TEST validating ${OUTPUT_RESOURCES_CSV_FILE}`);
+        // const resourcesCsvText = fs.readFileSync(OUTPUT_RESOURCES_CSV_FILE, 'utf-8');
 
         // validate matches and actions in resources.json
-        logger.info(`TEST validating ${OUTPUT_RESOURCES_JSON_FILE}`);
-        const rawData = fs.readFileSync(OUTPUT_RESOURCES_JSON_FILE, 'utf-8');
-        const resourceList = JSON.parse(rawData);
-        const resources = Object.fromEntries(resourceList.map((r: any) => [r.resourceId, r]));
-        expect(resourceList.length).to.equal(10); // number of resources
+        // logger.info(`TEST validating ${OUTPUT_RESOURCES_JSON_FILE}`);
+        // const rawData = fs.readFileSync(OUTPUT_RESOURCES_JSON_FILE, 'utf-8');
+        // const resourceList = JSON.parse(rawData);
+        // const resources = Object.fromEntries(resourceList.map((r: any) => [r.resourceId, r]));
+        // expect(resourceList.length).to.equal(10); // number of resources
 
-        expect(resources['i-0c688d35209d7f436'].resourceState).to.equal('running');
-        expect(resources['i-0c688d35209d7f436'].metadata.matches.length).to.equal(1);
-        expect(resources['i-0c688d35209d7f436'].metadata.matches[0].name).to.equal('everything off (p1)');
-        expect(resources['i-0c688d35209d7f436'].metadata.actionNames.length).to.equal(1);
-        expect(resources['i-0c688d35209d7f436'].metadata.actionNames[0]).to.equal('StopAction');
+        // expect(resources['i-0c688d35209d7f436'].resourceState).to.equal('running');
+        // expect(resources['i-0c688d35209d7f436'].metadata.matches.length).to.equal(1);
+        // expect(resources['i-0c688d35209d7f436'].metadata.matches[0].name).to.equal('everything off (p1)');
+        // expect(resources['i-0c688d35209d7f436'].metadata.actionNames.length).to.equal(1);
+        // expect(resources['i-0c688d35209d7f436'].metadata.actionNames[0]).to.equal('StopAction');
 
-        expect(resources['i-05b6baf37fc8f9454'].resourceState).to.equal('running');
-        expect(resources['i-05b6baf37fc8f9454'].metadata.matches.length).to.equal(2);
-        expect(resources['i-05b6baf37fc8f9454'].metadata.actionNames).to.equal(undefined);
+        // expect(resources['i-05b6baf37fc8f9454'].resourceState).to.equal('running');
+        // expect(resources['i-05b6baf37fc8f9454'].metadata.matches.length).to.equal(2);
+        // expect(resources['i-05b6baf37fc8f9454'].metadata.actionNames).to.equal(undefined);
       }).then(done, done);
     }
   });
