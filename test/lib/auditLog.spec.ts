2b26282987922f8aba236ccd2dc8f27eba4944ae
alutman-innablr <63215484+alutman-innablr@users.noreply.github.com>
2024-02-27 15:40:31 +1100

    Minor refactor of object log classes (#221)

--- test/lib/auditLog.spec.ts
+++ test/lib/auditLog.spec.ts
@@ -50,7 +50,7 @@ describe('Validate auditLog', function () {
       new FakeActionAuditEntry('green', 'dosomething', 'another reason'),
       new FakeActionAuditEntry('blue', 'dosomething else', 'random'),
     ];
-    await new ObjectLogCsv(new ActionAuditTable(ACCOUNT_CONFIG, entries, true), AUDIT_LOG_CONFIG.csv, false).process();
+    await new ObjectLogCsv(new ActionAuditTable(ACCOUNT_CONFIG, entries, true), AUDIT_LOG_CONFIG.csv).process();
 
     expect(fs.existsSync(AUDIT_LOG_CONFIG.csv.file)).to.be.true;
 
