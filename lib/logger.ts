8e7793b41b4173de78f47f4f41da773b5faaae74
Alex Lutman <alex.lutman@innablr.com.au>
2024-03-06 12:49:26 +1100

    Update JSON log structure

--- lib/logger.ts
+++ lib/logger.ts
@@ -11,11 +11,84 @@ const logLevels: { [key: string]: number } = {
   fatal: 6,
 };
 
+const logLevelsToConsole: { [key: number]: (...args: any) => any } = {
+  0: console.trace,
+  1: console.trace,
+  2: console.debug,
+  3: console.info,
+  4: console.warn,
+  5: console.error,
+  6: console.error,
+};
+
 export interface RevolverLogObject {
   accountId?: string;
   accountName?: string;
   pluginName?: string;
   driverName?: string;
+}
+
+/**
+ * Restructure JSON logs to behave better in JSON ingestors like cloudwatch.
+ *
+ * Writes JSON logs to the appropriate console.{level} function and converts any configured log prefixes
+ * to object keys if they have a ':' separator. Splits out string messages and object messages to message and data.
+ * @param log
+ */
+function restructureJsonLog(log: any) {
+  if (log == undefined) return;
+  const positionalEntries: string[] = Object.keys(log).filter((k: any) => !isNaN(k));
+
+  const prefixEntries: string[] = positionalEntries.slice(0, logger.settings.prefix.length);
+  const processedPrefix: { [key: string]: boolean } = {};
+  const prefixes: { [key: string]: string } = {};
+
+  // Move prefixed entries to true keys if they have a name
+  prefixEntries.forEach((k) => {
+    const segments = log[k].split(':');
+    if (segments.length > 1) {
+      prefixes[segments[0]] = segments.slice(1).join(':');
+      processedPrefix[k] = true;
+    }
+  });
+
+  const data: any[] = [];
+
+  // create unified message
+  const message: string = positionalEntries
+    .filter((k) => !processedPrefix[k])
+    .filter((k) => {
+      // arrays can be string printed
+      // move objects to data array
+      if (typeof log[k] === 'object' && !Array.isArray(log[k])) {
+        data.push(log[k]);
+        return false;
+      }
+      return true;
+    })
+    .map((k) => log[k])
+    .join(' ');
+
+  positionalEntries.forEach((k: string) => delete log[k]);
+
+  // assemble new object
+  const messageObj: { [key: string]: any } = {
+    message: message,
+  };
+  if (data.length > 0) {
+    messageObj.data = data;
+  }
+
+  // write to log level specific console.log
+  const outputLog = logLevelsToConsole[log[logger.settings.metaProperty].logLevelId] || console.log;
+
+  outputLog(
+    JSON.stringify({
+      ...messageObj,
+      ...prefixes,
+      ...log,
+    }),
+  );
 }
 
 export const logger = new Logger<RevolverLogObject>({
@@ -25,4 +98,9 @@ export const logger = new Logger<RevolverLogObject>({
   prettyLogTimeZone: environ.prettyLogTimeZone,
   minLevel: logLevels[environ.logLevel],
   hideLogPositionForProduction: logLevels[environ.logLevel] > 2,
+  overwrite: {
+    transportJSON: (logObjWithMeta: any) => {
+      restructureJsonLog(logObjWithMeta);
+    },
+  },
 });
