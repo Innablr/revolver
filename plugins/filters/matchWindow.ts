6b135d8c4a97fbb8c192b6a9df3ea32baf655103
Roberts, Simon <Simon.Roberts@anz.com>
2024-05-23 14:13:16 +1000

    Resolve a couple of lint issues

--- plugins/filters/matchWindow.ts
+++ plugins/filters/matchWindow.ts
@@ -1,11 +1,10 @@
 import { ToolingInterface } from '../../drivers/instrumentedResource';
 import dateTime from '../../lib/dateTime';
-import { arrayToOr, Filter, FilterCtor, StringCompareOptions } from './index';
+import { arrayToOr, Filter, FilterCtor } from './index';
 import { DateTime as LuxonDateTime } from 'luxon';
 
 export default class FilterMatchWindowStart implements Filter, FilterCtor {
   static readonly FILTER_NAME = 'match_window';
-  // private compareOptions: StringCompareOptions;
   private startTime: LuxonDateTime | undefined;
   private endTime: LuxonDateTime | undefined;
 
@@ -27,6 +26,8 @@ export default class FilterMatchWindowStart implements Filter, FilterCtor {
       }
     });
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   matches(resource: ToolingInterface): boolean {
     const now = dateTime.getTime();
     if (this.startTime && now < this.startTime) {
@@ -35,7 +36,7 @@ export default class FilterMatchWindowStart implements Filter, FilterCtor {
     if (this.endTime && now > this.endTime) {
       return false; // too late
     }
-    if (! this.startTime && ! this.endTime ) {
+    if (!this.startTime && !this.endTime) {
       return false; // no start, and no end = no match
     }
     return true;
