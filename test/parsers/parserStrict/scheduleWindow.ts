c09e5efe82619300dedf43ce2a9148d4a306ab78
Roberts, Simon <Simon.Roberts@anz.com>
2024-07-08 10:58:39 +1000

    Use an explicit datetime for testing rather than Now. Add checks for partial schedules (eg: Stop=23:00).

--- test/parsers/parserStrict/scheduleWindow.ts
+++ test/parsers/parserStrict/scheduleWindow.ts
@@ -2,29 +2,45 @@ import getParser from '../../../plugins/parsers/index.js';
 import { expect } from 'chai';
 import { DateTime, Interval } from 'luxon';
 
-const timeNow = DateTime.now();
-
 it('Strict parser calculates coverage correctly', async function () {
   const testInterval = 15; // number of minutes between samples
   const testWindow = { days: 7 }; // over what interval to sample
+  const startTime = DateTime.utc(2017, 3, 12, 0, 0, 0, 0);
+  const interval = Interval.fromDateTimes(startTime, startTime.plus(testWindow));
+  const testTimes = interval.splitBy({ minutes: testInterval }).map((d) => d.start);
 
+  // Schedule String, Minutes per Week, Start Running
   const cases = [
-    ['24x7', 24 * 7 * 60],
-    ['24x5', 24 * 5 * 60],
-    ['0x7', 0],
-    ['Start=08:30;Stop=09:00', 30 * 7],
-    ['Start=08:30|mon-fri;Stop=09:00|mon-fri', 30 * 5],
-    ['Start=09:00|mon-fri;Stop=17:00|mon-fri', 8 * 60 * 5],
-    ['xxx', 0],
+    ['24x7', 24 * 7 * 60, false],
+    ['24x5', 24 * 5 * 60, false],
+    ['0x7', 0, false],
+    ['Start=08:30;Stop=09:00', 30 * 7, false],
+    ['Start=08:30|mon-fri;Stop=09:00|mon-fri', 30 * 5, false],
+    ['Start=09:00|mon-fri;Stop=17:00|mon-fri', 8 * 60 * 5, false],
+    ['xxx', 0, false],
+    ['xxx', 24 * 7 * 60, true],
+    ['Stop=23:00', 23 * 60, true], // run until 11pm
+    ['Stop=23:00', 0, false], // stopped, still stopped after 11pm
+    ['Start=23:00', 60 + 24 * 6 * 60, false], // 60 minutes at end of day 1, then 24h/d after
+    ['Start=23:00', 24 * 7 * 60, true], // started, still started after 11pm
+    // currently not supported - see https://github.com/Innablr/revolver/issues/382
+    // ['Start=08:30|mon;Stop=09:00|mon', 30],
+    // ['Start=08:30|mon,thu-fri;Stop=09:00|mon,thu-fri', 30 * 3],
+    // ['Start=08:30|mon-tue,thu-fri;Stop=09:00|mon-tue,thu-fri', 30 * 4],
+    // ['Start=08:30|mon-wed,fri;Stop=09:00|mon-tue,thu-fri', 30 * 4],
   ];
 
   const strictParser = await getParser('strict');
-  const interval = Interval.fromDateTimes(timeNow, timeNow.plus(testWindow));
-  const startTimes = interval.splitBy({ minutes: testInterval }).map((d) => d.start);
-  cases.forEach(([tag, answer]) => {
-    const numMinutes = startTimes.reduce((uptime, t) => {
+  cases.forEach(([tag, answer, startRunning]) => {
+    let isRunning = startRunning;
+    const numMinutes = testTimes.reduce((uptime, t) => {
       const [action] = strictParser(tag, t);
-      return action == 'START' ? uptime + testInterval : uptime;
+      if (action == 'START') {
+        isRunning = true;
+      } else if (action == 'STOP') {
+        isRunning = false;
+      }
+      return isRunning ? uptime + testInterval : uptime;
     }, 0);
     expect(numMinutes).to.equal(answer, `Checking >${tag}<`);
   });
