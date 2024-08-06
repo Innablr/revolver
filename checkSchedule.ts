ea6c907572736869e1c10f4265c9f41cc502c7fb
Simon Roberts <lyricnz@gmail.com>
2024-08-07 08:59:45 +1000

    Get schedule-viewer working

--- checkSchedule.ts
+++ checkSchedule.ts
@@ -10,43 +10,34 @@ type ScheduleResults = Map<DateTime<true>, boolean>;
 
 const TEST_INTERVAL = 15; // number of minutes between samples
 const TEST_WINDOW = { days: 7 }; // over what interval to sample
+const DATE_FORMAT = 'ccc T';
+/**
+ * Evaluate the schedule every `interval` within the `window` and return a list of times:results.
+ * @param schedule a Revolver schedule string
+ * @param startTime when to start the test window
+ * @returns a Map of DateTime to boolean representing "matches schedule"
+ */
+async function runSchedule(schedule: string, startTime: DateTime): Promise<ScheduleResults> {
+  // determine all the test times across the window
+  const interval = Interval.fromDateTimes(startTime, startTime.plus(TEST_WINDOW));
+  const testTimes = interval.splitBy({ minutes: TEST_INTERVAL }).map((d) => d.start);
 
-// return isRunning an array of 7 * 24 * 4 = 672 intervals, each 15 minutes long
-class ScheduleRunner {
-  isRunning: boolean;
-  constructor() {
-    this.isRunning = false;
-  }
+  // load the parser
+  const strictParser = await getParser('strict');
 
-  /**
-   * Simulate a week of running against the given schedule, and return the results
-   * @param schedule a string representing the schedule
-   * @param startTime the start of the 7-day window
-   * @returns Map<DateTime, boolean> where the key is the time, and the value is whether the schedule is running
-   */
-  async runSchedule(schedule: string, startTime: DateTime): Promise<ScheduleResults> {
-    const interval = Interval.fromDateTimes(startTime, startTime.plus(TEST_WINDOW));
-    const testTimes = interval.splitBy({ minutes: TEST_INTERVAL }).map((d) => d.start);
+  // evaluate the parser across every test time
+  const results: ScheduleResults = new Map();
+  testTimes.forEach((t) => {
+    const [action] = strictParser(schedule, t);
+    results.set(t!, action == 'START');
+  });
 
-    const strictParser = await getParser('strict');
-    const results: ScheduleResults = new Map();
-    testTimes.forEach((t) => {
-      const [action] = strictParser(schedule, t);
-      if (action == 'START') {
-        this.isRunning = true;
-      } else if (action == 'STOP') {
-        this.isRunning = false;
-      }
-      results.set(t!, this.isRunning);
-    });
-
-    return results;
-  }
+  return results;
 }
 
-export default function listComprehension<T>(list: T[], callback: (item: T) => boolean): T[] {
-  return list.filter(callback).map((item) => item);
-}
+const everyNth = (arr: any[], nth: number) => arr.filter((e, i) => i % nth === nth - 1);
+
+const zeroPad = (num: any, places: number) => String(num).padStart(places, '0')
 
 /**
  * Validate the configuration provided by $CONFIG_FILE environment variable
@@ -54,45 +45,30 @@ export default function listComprehension<T>(list: T[], callback: (item: T) => b
  */
 async function validateConfig(): Promise<object> {
   // Note: configuration is not especially strict (unknown keys are ignored/stripped)
-  const config = await (environ.configPath
-    ? RevolverConfig.readConfigFromFile(environ.configPath)
-    : RevolverConfig.readConfigFromS3(environ.configBucket!, environ.configKey!));
+  // const config = await (environ.configPath
+  //   ? RevolverConfig.readConfigFromFile(environ.configPath)
+  //   : RevolverConfig.readConfigFromS3(environ.configBucket!, environ.configKey!));
 
-  const sr = new ScheduleRunner();
   const startTime = DateTime.utc(2017, 3, 12, 0, 0, 0, 0);
-  const stuff = await sr.runSchedule('Start=08:30;Stop=17:30', startTime);
+  const stuff = await runSchedule('Start=08:30|mon-fri;Stop=17:30|mon-fri', startTime);
 
   const keys = Array.from(stuff.keys());
   const chunkSize = (24 * 60) / TEST_INTERVAL;
+
+  // make an index row - each hour is '|hh '
+  const blah = everyNth(keys.splice(0, chunkSize), 4);
+  const header = blah.map((t) => '|' + zeroPad(t.hour, 2)).join(' ');
+
+  // Label should be "Mon 00:00"
+
+  logger.info('%s: %s', keys[0].toFormat(DATE_FORMAT), header);
   for (let i = 0; i < keys.length; i += chunkSize) {
-    const chunk = keys.slice(i, i + chunkSize);
-    // do whatever
-    logger.info('Chunk: %s', chunk[0]);
-    const evenNumbers = listComprehension(chunk, (x) => x % 2 === 0);
-    // let s;
-    // for (const key of chunk) {
-    //   logger.info('   %s: %s', key, stuff.get(key));
-    // }
+    const chunk = keys.slice(i, i + chunkSize); // a chunk of keys
+    const str = chunk.map((t) => (stuff.get(t) ? 'Y' : ' ')).join(''); // an array of Y/N
+    logger.info('%s: %s', chunk[0].toFormat(DATE_FORMAT), str);
   }
 
-  //   logger.info('Result: %s', stuff);
-
-  //   const interval = Interval.fromDateTimes(startTime, startTime.plus(TEST_WINDOW));
-  //   const testDays = interval.splitBy({ days: 1 }).map((d) => d.start);
-  //   for (const day of testDays) {
-  //     logger.info('Day: %s', day);
-
-  //     const interval2 = Interval.fromDateTimes(day!, day!.plus({ days: 1 }));
-  //     const testTimes2 = interval2.splitBy({ minutes: TEST_INTERVAL }).map((d) => d.start);
-  //     logger.info('   Tests: %s', testTimes2);
-
-  // }
-
-  //   const testTimes = interval.splitBy({ minutes: TEST_INTERVAL }).map((d) => d.start);
-  //   logger.info('interval: %s', interval);
-  //   logger.info('testDays: %s', testDays);
-
-  return config;
+  return {};
 }
 
 // Given a configuration file,
