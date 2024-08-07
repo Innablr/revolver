19b2669147e6bedd35d05dcf56c484749da62b6a
Roberts, Simon <Simon.Roberts@anz.com>
2024-08-08 09:54:24 +1000

    Start work on matcher-checker

--- checkSchedule.ts
+++ checkSchedule.ts
@@ -4,15 +4,23 @@ import environ from './lib/environ.js';
 import { RevolverConfig } from './lib/config.js';
 import { DateTime, Interval } from 'luxon';
 import getParser from './plugins/parsers/index.js';
+import yaml from 'js-yaml';
+import * as fs from 'node:fs';
 
 // a set of results for each 15 minute interval over a week
 type ScheduleResults = Map<DateTime<true>, boolean>;
 
 const TEST_INTERVAL = 15; // number of minutes between samples
 const TEST_WINDOW = { days: 7 }; // over what interval to sample
-const DATE_FORMAT = 'ccc T';
+const DATE_FORMAT = 'ccc DD T'; // 'Sun 12 Mar 2017'
+
+
+const everyNth = (arr: any[], nth: number) => arr.filter((e, i) => i % nth === nth - 1);
+
+const zeroPad = (num: any, places: number) => String(num).padStart(places, '0')
+
 /**
- * Evaluate the schedule every `interval` within the `window` and return a list of times:results.
+ * Evaluate the schedule for every interval in the window and return a list of time:results.
  * @param schedule a Revolver schedule string
  * @param startTime when to start the test window
  * @returns a Map of DateTime to boolean representing "matches schedule"
@@ -35,38 +43,68 @@ async function runSchedule(schedule: string, startTime: DateTime): Promise<Sched
   return results;
 }
 
-const everyNth = (arr: any[], nth: number) => arr.filter((e, i) => i % nth === nth - 1);
+/**
+ * Evaluate the schedule every `interval` within the `window` and print a table with results.
+ * @param schedule a Revolver schedule string
+ * @param startTime when to start the test window
+ * @returns a Map of DateTime to boolean representing "matches schedule"
+ */
+async function showSchedule(schedule: string, startTime: DateTime) {
+  const results = await runSchedule(schedule, startTime);
 
-const zeroPad = (num: any, places: number) => String(num).padStart(places, '0')
+  // create a header marking every hour: '|hh '
+  const keys = Array.from(results.keys());
+  const chunkSize = (24 * 60) / TEST_INTERVAL; // one day
+  const hourlyTimes = everyNth(keys.slice(0, chunkSize), 4);
+  const header = hourlyTimes.map((t) => '|' + zeroPad(t.hour, 2)).join(' ');
+
+  // emit a table describing the uptime window in the week
+  console.log('SCHEDULE: %s', schedule);
+  console.log('%s: %s', keys[0].toFormat(DATE_FORMAT), header);
+  for (let i = 0; i < keys.length; i += chunkSize) {
+    const chunk = keys.slice(i, i + chunkSize); // a chunk of keys
+    const str = chunk.map((t) => (results.get(t) ? 'Y' : ' ')).join(''); // an array of Y/N
+    console.log("%s: %s", chunk[0].toFormat(DATE_FORMAT), str);
+  }
+
+  const count = Array.from(results.values()).filter((value) => value).length;
+  const totalHours = results.size * TEST_INTERVAL / 60;
+  const uptime = count * TEST_INTERVAL / 60; // in hours
+  const uptimePercent = (uptime / (24 * 5)) * 100; // in percent
+  console.log("UPTIME %f/%d hours (%f%%)", uptime.toFixed(1), totalHours, uptimePercent.toFixed(1));
+}
+
+async function checkMatchers() {
+  // 2. show which resources would match a given config+resources
+  const config = await (environ.configPath
+    ? RevolverConfig.readConfigFromFile(environ.configPath)
+    : RevolverConfig.readConfigFromS3(environ.configBucket!, environ.configKey!));
+
+  // Override the schedule to test, and the list of resources
+  const resourcesDir = '/Users/robers19/CloudFinOps/cloudsnooze-reporting/anz-cloudsnooze-nonprod/output/2024/08/07';
+  const resourcesFile = 'resources.aws-ecp-main-workload-bow-nonprod-ap-southeast-2.json';
+  const accountDir = '/Users/robers19/CloudFinOps/cloudsnooze-config/nonprod/accounts';
+  const accountFile = 'aws-ecp-main-workload-bow-nonprod.yaml';
+
+  const accountConfigYaml = fs.readFileSync(accountDir + '/' + accountFile, 'utf8')
+  const accountConfig: any = yaml.load(accountConfigYaml);
+
+  // await main(config);
+  config.defaults.settings.resourceLog = undefined;
+  config.defaults.settings.auditLog = undefined;
+  config.defaults.plugins.powercycleCentral!.configs[0].matchers = accountConfig!.matchers;
+}
 
 /**
  * Validate the configuration provided by $CONFIG_FILE environment variable
  * @returns a parsed and validated configuration object
  */
 async function validateConfig(): Promise<object> {
-  // Note: configuration is not especially strict (unknown keys are ignored/stripped)
-  // const config = await (environ.configPath
-  //   ? RevolverConfig.readConfigFromFile(environ.configPath)
-  //   : RevolverConfig.readConfigFromS3(environ.configBucket!, environ.configKey!));
+  // 1. emit a diagram showing the effect of a schedule
+  // const startTime = DateTime.utc(2017, 3, 12, 0, 0, 0, 0);
+  // showSchedule('Start=09:00|mon-fri;Stop=18:00|mon-fri', startTime);
 
-  const startTime = DateTime.utc(2017, 3, 12, 0, 0, 0, 0);
-  const stuff = await runSchedule('Start=08:30|mon-fri;Stop=17:30|mon-fri', startTime);
-
-  const keys = Array.from(stuff.keys());
-  const chunkSize = (24 * 60) / TEST_INTERVAL;
-
-  // make an index row - each hour is '|hh '
-  const blah = everyNth(keys.splice(0, chunkSize), 4);
-  const header = blah.map((t) => '|' + zeroPad(t.hour, 2)).join(' ');
-
-  // Label should be "Mon 00:00"
-
-  logger.info('%s: %s', keys[0].toFormat(DATE_FORMAT), header);
-  for (let i = 0; i < keys.length; i += chunkSize) {
-    const chunk = keys.slice(i, i + chunkSize); // a chunk of keys
-    const str = chunk.map((t) => (stuff.get(t) ? 'Y' : ' ')).join(''); // an array of Y/N
-    logger.info('%s: %s', chunk[0].toFormat(DATE_FORMAT), str);
-  }
+  checkMatchers();
 
   return {};
 }
