d5b0a7b973d30aef0f53c063e739fab6bb4fb1a5
Roberts, Simon <Simon.Roberts@anz.com>
2024-09-02 11:42:24 +1000

    Add Sinon for spies, stubs and mocks. Validate getAwsClientForAccount()

--- test/lib/awsConfig.spec.ts
+++ test/lib/awsConfig.spec.ts
@@ -1,26 +1,42 @@
 import { expect } from 'chai';
 import { getAwsClientForAccount, getAwsConfig } from '../../lib/awsConfig.js';
 import { EC2Client } from '@aws-sdk/client-ec2';
-import sinon from 'sinon'; // Import the 'sinon' library
-import assume from '../../lib/assume.js';
-import { AwsCredentialIdentity as Credentials, Provider } from '@aws-sdk/types';
+import sinon from 'sinon';
+import { STS } from '@aws-sdk/client-sts';
+import environ from '../../lib/environ.js';
 
-describe('Validate auditLog', function () {
+describe('Validate getAwsConfig', function () {
   // check getAwsConfig
   const config = getAwsConfig('ap-southeast-2', { accessKeyId: 'blah', secretAccessKey: 'blah' });
   expect(config).to.be.an('object');
   expect(config.region).to.equal('ap-southeast-2');
 
-  it('Check ObjectLogCsv audit', async function () {
-    // check getAwsClientForAccount
-    const fakeCreds: Credentials = { accessKeyId: 'blah', secretAccessKey: 'blah' };
-    const stub = sinon.stub(assume, 'connectTo').resolves(fakeCreds);
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  it('Check getAwsClientForAccount', async function () {
+    // stub STS.assumeRole
+    environ.httpsProxy = 'https://localhost:1234';
+    const stsAssumeRoleStub = sinon
+      .stub(STS.prototype, 'assumeRole')
+      .resolves({ Credentials: { accessKeyId: 'blah', Expiration: tomorrow } });
+
+    // check getAwsClientForAccount (fresh)
     const client = await getAwsClientForAccount(EC2Client, {
       assumeRoleArn: 'arn:aws:iam::123456789012:role/blah',
       region: 'ap-southeast-2',
     });
-    expect(stub.calledOnce).to.be.true;
-    expect(client).to.be.an('object');
+    expect(stsAssumeRoleStub.calledOnce).to.be.true;
     expect(client).to.be.instanceOf(EC2Client);
+
+    // check getAwsClientForAccount (cache)
+    const client2 = await getAwsClientForAccount(EC2Client, {
+      assumeRoleArn: 'arn:aws:iam::123456789012:role/blah',
+      region: 'ap-southeast-2',
+    });
+    expect(stsAssumeRoleStub.calledOnce).to.be.true; // no further call (cache)
+    expect(client2).to.be.instanceOf(EC2Client); // still
+
+    stsAssumeRoleStub.restore(); // should be finally
   });
 });
