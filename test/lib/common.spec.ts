ec4f963762feb54023151f5c3c66234db874b756
Roberts, Simon <Simon.Roberts@anz.com>
2024-09-02 12:12:12 +1000

    Add tests for utility functions

---
+++ test/lib/common.spec.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import { chunkArray, makeResourceTags, paginateAwsCall, unique } from '../../lib/common.js';
+
+describe('Validate utility functions', function () {
+  it('Check getAwsClientForAccount', async function () {
+    function* paginatorFunc(config: any, request: any) {
+      yield { what: 'a', another: 1 };
+      yield { what: 'b', thing: 2 };
+      yield { what: 'c' };
+    }
+    const stuff = await paginateAwsCall(paginatorFunc, { client: 'client' }, 'what', { params: 'params' });
+    expect(stuff).to.be.an('array');
+    expect(stuff).to.have.lengthOf(3);
+    expect(stuff).to.deep.equal(['a', 'b', 'c']);
+  });
+
+  // chunkArray
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const chunks = chunkArray(alphabet, 5);
+  expect(chunks).to.be.an('array');
+  expect(chunks).to.have.lengthOf(6);
+  expect(chunks[0]).to.have.lengthOf(5);
+  expect(chunks[5]).to.have.lengthOf(1);
+  expect(chunks.flat()).to.have.lengthOf(26);
+  expect(chunks.flat()).to.deep.equal(alphabet);
+
+  // unique
+  const things = 'abcdaefghia'.split('');
+  const uniqueThings = unique(things);
+  expect(uniqueThings).to.have.lengthOf(9);
+  expect(uniqueThings).to.deep.equal('abcdefghi'.split(''));
+
+  // makeResourceTags
+  const tagList = [
+    { Key: 'a', Value: '1' },
+    { Key: 'b', Value: '2' },
+    { Key: 'c', Value: '3' },
+  ];
+  const tags = makeResourceTags(tagList);
+  expect(tags).to.be.an('object');
+  expect(tags).to.have.property('a', '1');
+  expect(tags).to.have.property('b', '2');
+  expect(tags).to.have.property('c', '3');
+  expect(tags).to.not.have.property('d');
+  const noTags = makeResourceTags(undefined);
+  expect(noTags).to.be.an('object');
+  expect(noTags).to.be.empty;
+});
