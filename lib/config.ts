9c6d822f6eb275c02cbd45b0270a3e0e34912555
Simon Roberts <lyricnz@users.noreply.github.com>
2025-07-26 09:26:45 +1000

    Disable flattenZodErrors

--- lib/config.ts
+++ lib/config.ts
@@ -7,18 +7,13 @@ import { Organizations, paginateListAccounts } from '@aws-sdk/client-organizatio
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import yaml from 'js-yaml';
 import { merge } from 'ts-deepmerge';
-import {
-  ZodError,
-  type ZodInvalidArgumentsIssue,
-  type ZodInvalidReturnTypeIssue,
-  type ZodInvalidUnionIssue,
-  ZodIssueCode,
-} from 'zod/v4';
+import { z } from 'zod/v4';
 import { getAwsConfig } from './awsConfig.js';
 import { paginateAwsCall } from './common.js';
 import { ConfigSchema } from './config-schema.js';
 import { ObjectLogJson } from './objectLog.js';
 
+/*
 function flattenZodErrors(ze: ZodError, depth: number): string[] {
   let lines: string[] = [];
   for (const zi of ze.errors) {
@@ -49,6 +44,7 @@ function flattenZodErrors(ze: ZodError, depth: number): string[] {
   }
   return lines;
 }
+*/
 
 // biome-ignore lint/complexity/noStaticOnlyClass: for reasons
 export class RevolverConfig {
@@ -87,9 +83,8 @@ export class RevolverConfig {
       logger.trace('Read Revolver config', config);
       return config;
     } catch (e: any) {
-      if (e instanceof ZodError) {
-        const ze = e as ZodError;
-        throw new Error(`ZodError: Failed to parse\n\t${flattenZodErrors(ze, 0).join('\n\t')}`);
+      if (e instanceof z.ZodError) {
+        throw new Error(`ZodError: Failed to parse\n${z.prettifyError(e)}`);
       } else {
         throw new Error(e);
       }
